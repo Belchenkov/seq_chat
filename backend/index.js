42adb000421b70eb890e4d11a166b44cf5236772
Aleksey Belchenkov <u608110@gmail.com>
2021-07-21 12:24:34 +0400

    Install Express

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
+const express = require('express');
 
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send('Home');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`);
+})
