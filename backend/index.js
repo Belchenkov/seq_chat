0867029f287cca3560528e940bc7be2331bb6c0c
Aleksey Belchenkov <u608110@gmail.com>
2021-07-21 12:39:58 +0400

    Create env config files

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
 const app = express();
+const config = require('./config/app');
 
 app.get('/', (req, res) => {
     return res.send('Home');
 });
 
-const port = 3000;
+app.get('/login', (req, res) => {
+    return res.send('Login');
+});
+
+const port = config.appPort || 3000;
 
 app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`);
